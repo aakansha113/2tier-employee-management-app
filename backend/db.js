a1a9e9c315453514abee336b45660e992676c067
aakansha113 <aakanshahujare@gmail.com>
2026-05-30 13:23:01 +0000

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,43 +1,15 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "mysql",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "employee_db",
   port: 3306,
 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
-// Health check query instead of just connection
-function waitForDB(retries = 30) {
-  const tryConnect = () => {
-    pool.query("SELECT 1", (err) => {
-      if (!err) {
-        console.log("✅ MySQL Connected Successfully");
-        return;
-      }
-
-      if (retries <= 0) {
-        console.error("❌ DB Connection Failed Permanently");
-
-        // Optional (GOOD for Kubernetes restart)
-        process.exit(1);
-      }
-
-      console.log(`⏳ Waiting for MySQL... retries left: ${retries}`);
-      retries--;
-
-      setTimeout(tryConnect, 3000);
-    });
-  };
-
-  tryConnect();
-}
-
-waitForDB();
 
 module.exports = pool;
