bea1d2a3b23e4404b72c31e9e8f19732bf4ed9fe
Aakansha Chandrakant Hujare <155349555+aakansha113@users.noreply.github.com>
2026-06-02 00:50:44 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -12,3 +12,9 @@ variable "ami_id" {
   description = "Ubuntu AMI ID"
   default     = "ami-0f918f7e67a3323f0"
 }
+
+variable "create_key_pair" {
+  description = "Create a new key pair or use existing one"
+  type        = bool
+  default     = false
+}
