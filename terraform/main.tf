3c18ede3fafba0d0294a5d9125f1af6ac3a1ce50
Aakansha Chandrakant Hujare <155349555+aakansha113@users.noreply.github.com>
2026-06-02 00:57:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,9 @@
 # ---------------- VPC ----------------
 
+# ---------------- VPC ----------------
+
 resource "aws_vpc" "employee_vpc" {
+  count      = var.create_vpc ? 1 : 0
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -8,10 +11,25 @@ resource "aws_vpc" "employee_vpc" {
   }
 }
 
+data "aws_vpc" "existing_vpc" {
+  count = var.create_vpc ? 0 : 1
+
+  filter {
+    name   = "tag:Name"
+    values = [var.existing_vpc_name]
+  }
+}
+
+locals {
+  vpc_id = var.create_vpc ?
+    aws_vpc.employee_vpc[0].id :
+    data.aws_vpc.existing_vpc[0].id
+}
+
 # ---------------- Public Subnet ----------------
 
 resource "aws_subnet" "employee_public_subnet" {
-  vpc_id                  = aws_vpc.employee_vpc.id
+  vpc_id                  = local.vpc_id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
@@ -24,7 +42,7 @@ resource "aws_subnet" "employee_public_subnet" {
 # ---------------- Internet Gateway ----------------
 
 resource "aws_internet_gateway" "employee_igw" {
-  vpc_id = aws_vpc.employee_vpc.id
+  vpc_id = local.vpc_id
 
   tags = {
     Name = "employee-igw"
@@ -34,7 +52,7 @@ resource "aws_internet_gateway" "employee_igw" {
 # ---------------- Route Table ----------------
 
 resource "aws_route_table" "employee_route_table" {
-  vpc_id = aws_vpc.employee_vpc.id
+  vpc_id = local.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -57,7 +75,7 @@ resource "aws_route_table_association" "employee_rta" {
 
 resource "aws_security_group" "employee_sg" {
   name   = "employee-app-sg"
-  vpc_id = aws_vpc.employee_vpc.id
+  vpc_id = local.vpc_id
 
   ingress {
     from_port   = 22
@@ -104,12 +122,27 @@ resource "aws_security_group" "employee_sg" {
     Name = "employee-sg"
   }
 }
+
 # ---------------- Key Pair ----------------
 
 resource "aws_key_pair" "employee_key" {
+  count      = var.create_key_pair ? 1 : 0
   key_name   = "employees-key"
   public_key = file("employees.pub")
 }
+
+data "aws_key_pair" "existing_key" {
+  count    = var.create_key_pair ? 0 : 1
+  key_name = "employees-key"
+}
+
+locals {
+  key_name = var.create_key_pair ?
+    aws_key_pair.employee_key[0].key_name :
+    data.aws_key_pair.existing_key[0].key_name
+}
+# ---------------- EC2 ----------------
+
 # ---------------- EC2 ----------------
 
 resource "aws_instance" "employee_app" {
@@ -118,9 +151,10 @@ resource "aws_instance" "employee_app" {
   subnet_id              = aws_subnet.employee_public_subnet.id
   vpc_security_group_ids = [aws_security_group.employee_sg.id]
 
-  key_name = aws_key_pair.employee_key.key_name
+  key_name = local.key_name
 
   user_data = file("script.sh")
+
   root_block_device {
     volume_size = 20
     volume_type = "gp3"
